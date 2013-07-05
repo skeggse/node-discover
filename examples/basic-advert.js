5badea2135c567cf1b3194211a92217567bbabcd
Eli Skeggs <skeggse@gmail.com>
2013-07-05 10:54:46 -0700

    Add description, switch to spaces instead of hard tabs
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- examples/basic-advert.js
+++ examples/basic-advert.js
@@ -1,6 +1,12 @@
-/*
+/**
+ * This is the most basic example of using Discover.
  * 
+ * In this example all we are interested in is advertising ourself to the
+ * network, and detecting when new nodes enter or leave the network. The master
+ * selection stuff happens behind the scenes but we can completely ignore it
+ * and just handle the events for nodes entering and leaving.
  * 
+ * See also: basic.js
  */
 
 var Discover = require("../");
@@ -8,16 +14,16 @@ var Discover = require("../");
 var d = new Discover();
 
 d.advertise({
-	http : "80",
-	random : Math.random()
+  http: "80",
+  random: Math.random()
 });
 
 d.on("added", function (obj) {
-	console.log("New node added to the network.");
-	console.log(obj);
+  console.log("New node added to the network.");
+  console.log(obj);
 });
 
 d.on("removed", function (obj) {
-	console.log("Node removed from the network.");
-	console.log(obj);
+  console.log("Node removed from the network.");
+  console.log(obj);
 });
--- examples/basic.js
+++ examples/basic.js
@@ -14,11 +14,11 @@ var Discover = require("../");
 var d = new Discover();
 
 d.on("added", function (obj) {
-	console.log("New node added to the network.");
-	console.log(obj);
+  console.log("New node added to the network.");
+  console.log(obj);
 });
 
 d.on("removed", function (obj) {
-	console.log("Node removed from the network.");
-	console.log(obj);
+  console.log("Node removed from the network.");
+  console.log(obj);
 });
